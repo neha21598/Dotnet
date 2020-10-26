4dc14e3816cc239c69d53262097b045d70cc6434
neha21598 <nehapatil3997@gmail.com>
2020-10-26 12:06:29 +0530

    Question 3_2

---
+++ Day 3/Question2/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Q2. Accept a number from user and pass it to static method of a struct calculator it should print all factor of a number.*/
+
+using System;
+
+namespace Day3_2
+{
+    struct Calculator
+    {
+        public static void Factorial(int num)
+        {
+            Console.WriteLine("Factors of a number are:");
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string num;
+            int f;
+            Console.WriteLine("Enter a number:");
+            num=Console.ReadLine();
+            bool a1 = int.TryParse(num, out f);
+            if (a1 == true)
+            {
+                Calculator.Factorial(f);
+            }
+            Console.ReadLine();
+        }
+    }
+}
