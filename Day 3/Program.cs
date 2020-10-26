fd6614a56de4d79694b4a26a555e2a05da5e5a3c
neha21598 <nehapatil3997@gmail.com>
2020-10-26 11:57:57 +0530

    Question 3_1

---
+++ Day 3/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Q1. Create a struct Math having two static method square and cube. Using struct variable call this method and print 
+ * square and cube of a number*/
+
+using System;
+
+namespace Day3_1
+{
+    struct Math
+    {
+        public static void square(int a)
+        {
+            Console.WriteLine("Sqaure is:{0}", a * a);
+        }
+
+        public static void cube(int a)
+        {
+            Console.WriteLine("Cube is:{0}", a* a * a);
+        }
+    
+        static void Main(string[] args)
+        {
+            Math.square(2);
+
+            Math.cube(2);
+            Console.ReadLine();
+        }
+    }
+}
