8ff558387bec351f02e39546becf5ebf688e4ead
neha21598 <nehapatil3997@gmail.com>
2020-10-26 12:26:05 +0530

    Question 3_5

---
+++ Day 3/Question5/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Q6. Create a class having instance member salary. Create a object of a class. Print data type of reference of a class also print
+ * parent class name.*/
+
+using System;
+
+namespace Day_3_5
+{
+    class program
+    {
+        double salary;
+
+        static void Main(string[] args)
+        {
+            program p1 = new program();
+            Console.WriteLine("Data type of reference is:{0}",p1.GetType());
+            Console.WriteLine("Base type of reference is:{0}", p1.GetType().BaseType);
+            Console.ReadLine();
+        }
+    }
+}
