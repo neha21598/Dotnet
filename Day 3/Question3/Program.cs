d70f40b4bc8b18d874a26fea9ef88c7a5c68112c
neha21598 <nehapatil3997@gmail.com>
2020-10-26 12:12:38 +0530

    Question 3_3

---
+++ Day 3/Question3/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Q3. Create struct emp having member name and salary, using constructor initialized member.
+ * Write display method which will display name and salary*/
+
+using System;
+
+namespace Day_3_3
+{
+   struct emp
+   {
+       string name;
+       double salary;
+
+       public emp(string name, double salary)
+       {
+           this.name = name;
+           this.salary = salary;
+       }
+
+       public void display()
+       {
+           Console.WriteLine("Hiii {0} your Salary is:{1}", name, salary);
+       }
+        
+       static void Main(string[] args)
+       {
+           emp e1 = new emp("Neha", 50000);
+           e1.display();
+           Console.ReadLine();
+       }
+    }
+}
