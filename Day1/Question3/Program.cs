757c9d797e601149b9a8f18a34dcd2968b3716e3
neha21598 <nehapatil3997@gmail.com>
2020-10-26 13:37:46 +0530

    Question 1_3

---
+++ Day1/Question3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Declare a variable of a type float and print square
+ of it after storing it in to a variable.
+*/
+
+using System;
+
+namespace Day1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 2.5f;
+            
+            float square = num1 *num1;
+            Console.WriteLine("square is:{0}", square);
+            Console.ReadLine();
+        }
+    }
+}
