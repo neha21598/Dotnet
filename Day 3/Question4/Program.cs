08ac6142b2e61a01be28654bfaa632a3119855b8
neha21598 <nehapatil3997@gmail.com>
2020-10-26 12:21:26 +0530

    Question 3_4

---
+++ Day 3/Question4/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Accept a number from user and display its type and parent class */
+
+using System;
+
+namespace Day_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f;
+            Console.WriteLine("Enter a number:");
+            string num;
+            num = Console.ReadLine();
+            bool a1=int.TryParse(num,out f);
+            Console.WriteLine("Type of number{0} is:{1}",f,f.GetType());//To print type
+            Console.WriteLine("Parent class of {0} is:{1}",f,f.GetType().BaseType);//To print parent class
+            Console.ReadLine();
+        }
+    }
+}
