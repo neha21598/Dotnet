ef82bfc8fd30fce04f6ce989103ee4c89d740694
neha21598 <nehapatil3997@gmail.com>
2020-10-26 11:35:29 +0530

    Question 4

---
+++ Day 4/Question4/Class1.cs
@@ -0,0 +1,33 @@
+﻿/* Q4. Create a class Printer with a private constructor. 
+Your application should give only one reference of the printer object.*/
+
+using System;
+
+namespace Day4_4_lib
+{
+    public class Printer
+    {
+        static Printer obj;
+        static Printer()
+        {
+            Console.WriteLine("WELCOME");
+        }
+
+        private Printer()
+        {
+            obj = null;
+        }
+
+        public static Printer demo()
+        {
+            if (obj == null)
+                obj = new Printer();
+            return obj;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("HIII");
+        }
+    }
+}
---
+++ Day 4/Question4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Day4_4_lib;
+
+namespace Day4_4_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p1 = Printer.demo();
+            p1.display();
+
+            Console.ReadLine();
+
+        }
+    }
+}
