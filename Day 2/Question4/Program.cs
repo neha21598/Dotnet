7642694e4157f899ca8782e605a5723df5a6f54d
neha21598 <nehapatil3997@gmail.com>
2020-10-26 13:07:12 +0530

    Question 2_4

---
+++ Day 2/Question4/Program.cs
@@ -0,0 +1,24 @@
+﻿/*  Accept two name from user and concat.*/
+
+using System;
+
+namespace Day2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1, name2;
+
+            Console.WriteLine("Enter First Name:");
+            name1=Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            name2 = Console.ReadLine();
+
+            Console.WriteLine("Full Name is:{0}{1}", name1, name2);
+            Console.ReadLine();
+
+        }
+    }
+}
