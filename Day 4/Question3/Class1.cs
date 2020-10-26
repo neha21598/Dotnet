ac7d0c92146d9023a25cdad8362c7f361cb99c26
neha21598 <nehapatil3997@gmail.com>
2020-10-26 11:13:49 +0530

    Question 3

---
+++ Day 4/Question3/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_4_3_lib
+{
+    public class Customer
+    {
+        int id;
+        string name;
+        double bill_amount;
+
+        public Customer()
+        { }
+
+        public void display()
+        {
+            Console.WriteLine("Default values are:");
+            Console.WriteLine("Id is: {0} \n Name is: {1} \n Bill Amount is: {2}", id, name, bill_amount);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Day 4/Question3/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Q3. Create a class customer having member Id, name,
+ billamount. When you create a object allow to have 
+default vale. Print default value of id,name, 
+billamount.*/
+
+using System;
+using Day_4_3_lib;
+
+namespace Day4_3_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.display();
+        }
+    }
+}
