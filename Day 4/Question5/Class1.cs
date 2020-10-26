45fdf2b99a0850aec5aaec7a7fe1fa1c4817e9f2
neha21598 <nehapatil3997@gmail.com>
2020-10-26 11:48:25 +0530

    Question 5

---
+++ Day 4/Question5/Class1.cs
@@ -0,0 +1,40 @@
+﻿/* Q5. Create a class calculator having  static method square, cube, absolute, floor,ceil who’s job is to find square, cube, 
+ * absolute (2.2 ==2)floor(eg. 2.7==O/P==2) , Ceil(eg 2.9==3) value respectively.*/
+
+using System;
+
+namespace Day4_5_lib
+{
+    public class Calculator
+    {
+        static Calculator()
+        {
+            Console.WriteLine("*********In Calculator Class***********");
+        }
+        public static void square(int a)
+        {
+            Console.WriteLine("Square is:{0}",a*a);
+        }
+
+        public static void cube(int a)
+        {
+            Console.WriteLine("Cube is:{0}",a*a*a);
+        }
+    
+        public static void absolute(int a)
+        {
+            Console.WriteLine("Absolute value  is:{0}",Math.Abs(a));
+        }
+    
+        public static void floor(double a)
+        {
+            Console.WriteLine("Floor value is:{0}",Math.Floor(a));
+        }
+    
+        public static void ceil(double a)
+        {
+            Console.WriteLine("Ceil value is:{0}",Math.Ceiling(a));
+        }
+    
+    }
+}
---
+++ Day 4/Question5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Day4_5_lib;
+
+namespace Day4_5_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.square(2);
+            Calculator.floor(6.09);
+            Calculator.ceil(6.2);
+            Calculator.cube(4);
+            Console.ReadLine();
+        }
+    }
+}
