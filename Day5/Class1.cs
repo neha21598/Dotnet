24d305ccf575c91169b6b13e85f2697e2d64360d
neha21598 <nehapatil3997@gmail.com>
2020-10-26 18:41:15 +0530

    Question 5_1

---
+++ Day5/Class1.cs
@@ -0,0 +1,36 @@
+﻿/* Q1. Create a class Employee having member variable Firstname, Lastname, Middlename.let your application create object even if 
+ * user does not enter Middlename. Create two constructor one take parameter for Firstname, Lastname and second constructor which
+ * will take all the parameter. Write display method which will display detail of the object.*/
+
+using System;
+
+namespace Day5_1_lib
+{
+    public class Employee
+    {
+        string fname, mname, lname;
+
+        public Employee()
+        {
+            Console.WriteLine("In No arg COnstructor");
+        }
+
+        public Employee(string fnm, string mnm, string lnm)
+        {
+            fname = fnm;
+            mname = mnm;
+            lname = lnm;
+        }
+
+        public Employee(string fnm, string lnm)
+        {
+            fname = fnm;
+            lname = lnm;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Hello {0} {1} {2}", fname, mname, lname);
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Day5_1_lib;
+
+namespace Day5_1_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ob =new Employee();
+            Employee ob1 = new Employee("Neha","Patil");
+            Employee ob2 = new Employee("Neha","Rajendra", "Patil");
+
+            ob.display();
+            ob1.display();
+            ob2.display();
+
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
