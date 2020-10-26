21c06cd198d0ead3d85ce72491c546b861aefcc7
neha21598 <nehapatil3997@gmail.com>
2020-10-26 13:24:06 +0530

    Question 2_6

---
+++ Day 2/Question5/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Q5. Accept a number from user and print square of it,
+ if in put is not valid give appropriate message
+*/
+
+using System;
+
+namespace Day2_5
+{
+    class Program
+    {
+        public void square(int a)
+        {
+            Console.WriteLine("Square of {0} is:{1}", a, a * a);
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+
+            string num;
+            int f;
+
+            Console.WriteLine("Enter a number:");
+
+            num = Console.ReadLine();
+            bool a1 = int.TryParse(num, out f);
+
+            if (a1 == true)
+            {
+                p1.square(f);
+            }
+
+            else
+            {
+                Console.WriteLine("Enter a valid number:");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 2/Question6/Program.cs
@@ -0,0 +1,25 @@
+﻿/*  Accept a number and stored it in byte data type check what happen if you enter 258?*/
+
+using System;
+
+namespace Day2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num;
+            byte f;
+
+            Console.WriteLine("Enter number:");
+
+            checked
+            {
+                num = Console.ReadLine();
+                bool a1 = byte.TryParse(num, out f);
+                Console.WriteLine(f);
+                Console.ReadLine();
+            }
+        }
+    }
+}
