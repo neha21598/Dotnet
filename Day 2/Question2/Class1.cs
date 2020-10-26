f61d780d9bf8d416647afdc71b9cda32de2d3141
neha21598 <nehapatil3997@gmail.com>
2020-10-26 12:42:58 +0530

    Question 2_2

---
+++ Day 2/Question2/Class1.cs
@@ -0,0 +1,19 @@
+﻿/*Create a class library which will return square, cube of a number. [Create private Assembly]*/
+
+using System;
+
+namespace Day_2_2_lib
+{
+    public class Class1
+    {
+        public void square(int a)
+        {
+            Console.WriteLine("Square of {0} is:{1}", a, a * a);
+        }
+
+        public void cube(int a)
+        {
+            Console.WriteLine("cube of {0} is:{1}", a, a * a * a);
+        }
+    }
+}
---
+++ Day 2/Question2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Day_2_2_lib;
+
+namespace Day_2_2_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.square(2);
+            c1.cube(2);
+            Console.ReadLine();
+        }
+    }
+}
