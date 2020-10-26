83092e831d0ebc803c552dc72816af0f49868aed
neha21598 <nehapatil3997@gmail.com>
2020-10-26 13:34:41 +0530

    Question 1_2

---
+++ Day1/Question2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Declare 2  variable of a type byte and print product of it.*/
+
+using System;
+
+namespace Day1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1 = 2;
+            byte num2 = 2;
+
+            byte product = (byte)(num1 * num2);
+            Console.WriteLine("Product is:{0}", product);
+            Console.ReadLine();
+
+        }
+    }
+}
