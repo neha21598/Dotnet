6203cc4763ceca86b42414f7b2d8e65910330045
neha21598 <nehapatil3997@gmail.com>
2020-10-28 20:36:58 +0530

    Question8_1

---
+++ Day8/Question1/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Q1. Create a static class Mymeth having two static  method
+	1. public int sqr(int n) :this method return square of a number
+	2. public double sqr(double s): this method return square of a number
+Create a entry point class and call this method.*/
+
+using   System;
+
+namespace Day8_1
+{
+    static class Mymath
+    {
+        public static int sqr(int n)
+        {
+            return n * n;
+        }
+        public static double sqr(double s)
+        {
+            return s * s;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            int res1=sqr(2);
+            Console.WriteLine("Square of 2 is:{0}", res1);
+
+            double res2=sqr(2.2);
+            Console.WriteLine("Square of 2.2 is:{0}", res2);
+
+            Console.ReadLine();
+        }
+    }
+}
