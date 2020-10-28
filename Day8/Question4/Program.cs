8f03ed3ce70f71193c259b9409a5aac80b31e886
neha21598 <nehapatil3997@gmail.com>
2020-10-28 20:38:08 +0530

    Question8_4

---
+++ Day8/Question4/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ Q4. Create a class swapdemo having method
+ 	Public void swap(int x, int y)
+Job of this method is to swap two number. Create a entry point class  who’s job is to call this method and print swapped data.*/
+
+
+using System;
+
+namespace Day8_4
+{
+    class swapdemo
+    {
+        public void swap(int x, int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine("Swapped Data is:");
+            Console.WriteLine("x={0}", x);
+            Console.WriteLine("y={0}", y);
+        }
+        static void Main(string[] args)
+        {
+            swapdemo s = new swapdemo();
+            s.swap(2, 3);
+
+            Console.ReadLine();
+        }
+    }
+}
