a5bcd30ce21f509e2c91fccbba56911e5edd867f
neha21598 <nehapatil3997@gmail.com>
2020-10-28 19:21:42 +0530

    Question 7_1

---
+++ Day7/Question1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Accept5 element in an array and sort ascending order*/
+
+using System;
+
+namespace Day7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarr = new int[5];
+            Console.WriteLine("Enter 5 elements:");
+            for(int i=0;i<5;i++)
+            {
+                myarr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Before Sorting Array Elements are:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(myarr[i] + "\t");
+            }
+
+            Array.Sort(myarr);
+            Console.WriteLine("\n After sorting Array Elements are:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(myarr[i] + "\t");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
