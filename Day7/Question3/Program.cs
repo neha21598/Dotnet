f7c98fe7882edecb5e54f4e3132a9665337085ab
neha21598 <nehapatil3997@gmail.com>
2020-10-28 19:23:33 +0530

    Question 7_3

---
+++ Day7/Question3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*  Accept 5 element in an array and copy it to another array.*/
+
+using System;
+
+namespace Day7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarr = new int[5];
+            int[] cpy = new int[5];
+
+            Console.WriteLine("Enter 5 elements:");
+            for (int i = 0; i < 5; i++)
+            {
+                myarr[i] = int.Parse(Console.ReadLine());
+            }
+
+            myarr.CopyTo(cpy, 0);
+
+            Console.WriteLine("\n Copied array elements are:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(cpy[i] + "\t");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
