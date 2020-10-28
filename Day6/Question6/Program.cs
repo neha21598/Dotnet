ba54188031d9292deb6218e3acb9599a9713d26f
neha21598 <nehapatil3997@gmail.com>
2020-10-28 19:32:04 +0530

    Question 6_6

---
+++ Day6/Question6/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_6_console
+{
+    class Program    {        static void Main(string[] args)        {            int sum = 0;            Console.WriteLine("Enter Size of Array");            int size=int.Parse(Console.ReadLine());            int[] arr = new int[size];            Console.WriteLine("Enter The Elements of an Array:");            for (int i = 0; i < size; i++)            {                arr[i] = int.Parse(Console.ReadLine());            }            for (int i = 0; i < size; i++)            {                sum = sum + arr[i];            }            int average = sum / (size);            Console.WriteLine("The Average of Each Element of Array is :{0}",average);                                    Console.ReadLine();        }    }}
+
