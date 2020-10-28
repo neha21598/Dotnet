6e1a7747865946d96b13b8f9775ae15c4f929bb2
neha21598 <nehapatil3997@gmail.com>
2020-10-28 19:30:29 +0530

    Question 6_4

---
+++ Day6/Question4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day_6_4_Console
+{
+    class Array_Matrix
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("Enter Size of Array");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            int[] sqrarr = new int[size];
+
+            Console.WriteLine("Enter The Elements of an Array:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                sum = sum + arr[i];
+            }
+
+            Console.WriteLine("The sum of Array is {0}", sum);
+
+            Console.WriteLine("The Square of Each Element of Array is :");
+            Console.Write("[");
+            for (int i = 0; i < size; i++)
+            {
+                sqrarr[i] = (arr[i]) * (arr[i]);
+                Console.Write(sqrarr[i] + ",");
+            }
+            Console.Write("]");
+            Console.ReadLine();
+
+        }
+    }
+}                      
