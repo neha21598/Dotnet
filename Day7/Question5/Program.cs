1c66e283484e4ce84bf90afcf86b0dd3c0b94860
neha21598 <nehapatil3997@gmail.com>
2020-10-28 19:25:45 +0530

    Question 7_5

---
+++ Day7/Question5/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Accept a name from user accept password and confirm password from user if password and confirm
+ * password string are same give message valid data else print invalid data.*/
+
+ 
+
+using System;
+
+namespace Day7_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Password:");
+            string pass = Console.ReadLine();
+
+            Console.WriteLine("Enter Confirm Password:");
+            string confirm_pass = Console.ReadLine();
+
+            if (object.Equals(pass, confirm_pass))
+                Console.WriteLine("VALID USERNAME AND PASSWORD");
+
+            else
+                Console.WriteLine("INVALID DATA");
+
+            Console.ReadLine();
+
+        }
+    }
+}
