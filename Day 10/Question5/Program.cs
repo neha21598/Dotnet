aac8aea0799cdec272dcb28b6dbe28356e37b2c0
neha21598 <nehapatil3997@gmail.com>
2020-11-09 17:20:01 +0530

    Question 10_5

---
+++ Day 10/Question5/Program.cs
@@ -0,0 +1,50 @@
+﻿/* . Create a interface Itaxtogov with method void paytax(double amt) create a class myindia, myeurope let this class implement this interface.
+ * In class myindia  paytax method will print tax amount which is 40% of the data passed in method. In class myeurope  paytax method will print tax amount 
+ * which is 30% of the data passed in method */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_5
+{
+    interface Itaxtogov
+    {
+        void paytax(double amt);
+    }
+
+    class myindia:Itaxtogov
+    {
+        public void paytax(double amt)
+        {
+            Console.WriteLine("Tax of India is:{0}",amt*0.4);
+        }
+    }
+
+    class myeurope : Itaxtogov
+    {
+        public void paytax(double amt)
+        {
+            Console.WriteLine("Tax of europe is:{0}",amt * 0.3);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myindia india = new myindia();
+            india.paytax(10000);
+
+            myeurope e = new myeurope();
+            e.paytax(10000);
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
