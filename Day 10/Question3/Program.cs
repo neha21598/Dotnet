8cfcef0622ecf83ada3f7fa6aa4e2bcb538c370c
neha21598 <nehapatil3997@gmail.com>
2020-11-09 17:17:31 +0530

    Question 10_3

---
+++ Day 10/Question3/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Create array of anonymous object displaying name and salary. Print all detail*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_3
+{
+    public class Employee
+    {
+   
+        string Name{get;set;}
+        double Salary { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp=new[] {new{Name="Neha",Salary=50000},new {Name="Pallavi",Salary=40000}};
+           
+            foreach (var e in emp)
+            {
+                Console.WriteLine("Name and Salary is:{0}   {1}",e.Name,e.Salary);
+               
+              
+
+            }
+         
+            Console.ReadLine();
+        }
+    }
+}
