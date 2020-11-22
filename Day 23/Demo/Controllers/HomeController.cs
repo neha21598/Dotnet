abef66ce8af7cb928dc1ef84c59935b767fd03a6
neha21598 <nehapatil3997@gmail.com>
2020-11-22 18:21:59 +0530

    Day 23 MVC Demo

---
+++ Day 23/Demo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day23_Demo.Models;
+
+namespace Day23_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        StudentLayer sl = new StudentLayer();
+
+        public ActionResult Index()
+        {
+            return View(sl.Display);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            sl.InsertData(s);
+            return View();
+        }
+
+    }
+}
---
+++ Day 23/Demo/Models/StudentLayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Day23_Demo.Models
+{
+    public class StudentLayer
+    {
+        public SqlConnection getConnection()
+        {
+            string connectionstring = @"Data Source=(localdb)\Projects;Initial Catalog=Student_Db;Integrated Security=True";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connectionstring;
+            return con;
+        }
+
+        public void InsertData(Student s)
+        {
+            int Totalrows = 0;
+
+            using (SqlConnection con = getConnection())
+            {
+                SqlCommand cmd = new SqlCommand("insert into Student values(@sname,@saddress)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@sname", s.Name);
+                cmd.Parameters.AddWithValue("@saddress", s.Address);
+
+                Totalrows = cmd.ExecuteNonQuery();
+                Console.WriteLine("Data inserted Successfully");
+            }
+        }
+
+        public List<Student> Display
+        {
+            get
+            {
+                List<Student> mylist = new List<Student>();
+
+                using (SqlConnection con = getConnection())
+                {
+                    SqlCommand cmd = new SqlCommand("Select * from Student", con);
+                    con.Open();
+
+                   SqlDataReader rdr= cmd.ExecuteReader();
+
+                   if (rdr.HasRows)
+                   {
+                       while (rdr.Read())
+                       {
+                           Student s = new Student();
+                          
+                           s.Name = rdr["Name"].ToString();
+                           s.Address = rdr["Address"].ToString();
+
+                           mylist.Add(s);
+
+                       }
+                   }
+                   return mylist;
+                }
+            }
+        }
+    }
+}
