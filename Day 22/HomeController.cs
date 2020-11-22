46aeed6cf1aaa6eee01e89e360d536cfe34e9003
neha21598 <nehapatil3997@gmail.com>
2020-11-22 10:37:07 +0530

    Day 22 Question1

---
+++ Day 22/HomeController.cs
@@ -0,0 +1,37 @@
+﻿/*Q1. Create a MVC application having hyperlink Home,
+ About us, Contact on click of each it should open 
+respective view file. Your link should be in a file 
+menubar.cshtml in a shared folder of a view folder. 
+Also put footer in footer.cshtml file. Include this
+ file using partial method in _Layout.cshtml*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day23_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
